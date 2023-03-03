5c0c7ae1680524a7cba625744126d88f93bf989d
Xavier Olivares <xm.olivares@gmail.com>
2023-03-02 21:21:13 -0500

    cookie permissions draft

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-let score = sessionStorage.getItem('score') || 0;
-let scoreUp = document.getElementById('score');
-let holes = Array.from(document.getElementsByClassName('hole'));
+let score = Number(getCookie('cookie_score') || sessionStorage.getItem('score')) || 0,
+    scoreUp = document.getElementById('score'),
+    holes = Array.from(document.getElementsByClassName('hole')),
+    cookieConsent = !!getCookie('cookie_consent') || false;
 
+/* Aligning visible and sessionStorage scores. */
 scoreUp.innerText = score;
+sessionStorage.setItem('score', score);
 
-function newHole () {
+function newHole() {
     let randomHole = Math.floor(Math.random() * 9);
     holes[randomHole].classList.toggle('mole');
 }
@@ -19,5 +22,37 @@ gameArea.addEventListener('click', function(clickEvent) {
         score++;
         scoreUp.innerText = score;
         sessionStorage.setItem('score', score);
+        setCookie('cookie_score', score);
     }
+});
+
+/* Cookie logic - pulled from https://www.tabnine.com/academy/javascript/how-to-set-cookies-javascript/ */
+let cookie_score = score;
+
+/* Set a Cookie */
+function setCookie(cName, cValue, expDays) {
+        let date = new Date();
+        date.setTime(date.getTime() + (expDays * 24 * 60 * 60 * 1000));
+        const expires = "expires=" + date.toUTCString();
+        document.cookie = cName + "=" + cValue + "; " + expires + "; path=/";
+}
+
+/* Get a Cookie */
+function getCookie(cName) {
+    const name = cName + "=";
+    const cDecoded = decodeURIComponent(document.cookie); //to be careful
+    const cArr = cDecoded .split('; ');
+    let res;
+    cArr.forEach(val => {
+        if (val.indexOf(name) === 0) res = val.substring(name.length);
+    })
+    return res;
+}
+
+/* Apply setCookie */
+const cookieConsentButton = document.getElementById('cookie-consent');
+cookieConsentButton.addEventListener('click', function(clickEvent) {
+    alert('Saving your score in the cookie!');
+    setCookie('cookie_consent', true, 1);
+    setCookie('cookie_score', cookie_score, 1);
 });
